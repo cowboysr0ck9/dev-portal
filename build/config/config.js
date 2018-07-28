4b123a00f302d3bb4f1b4e2d69c12a133fa89907
Tyler Eads <tyler.eads9@gmail.com>
2018-07-28 11:28:51 -0400

    Removed Build from .gitignore

--- build/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEV_ENV = {
-    MONGO_URI: "mongodb://dev_admin:Password#2018@ds014368.mlab.com:14368/rest-api",
-};
