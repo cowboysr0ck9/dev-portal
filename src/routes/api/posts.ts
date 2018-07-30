3021380585f3d3ea46eedc295f1f5ed949294afd
Tyler Eads <tyler.eads9@gmail.com>
2018-07-29 20:17:57 -0400

    Added Post & Profile route information

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -3,7 +3,9 @@ import { Request, Response } from "express";
 const router = express.Router();
 
 
-
+// @Route GET api/posts/test
+// @Desc Test posts route
+// @Access Public route
 router.get('/test', (req: Request, res: Response) => res.json({ msg: 'Posts works' }));
 
 
--- src/routes/api/profile.ts
+++ src/routes/api/profile.ts
@@ -3,7 +3,9 @@ import { Request, Response } from "express";
 const router = express.Router();
 
 
-
+// @Route GET api/profile/test
+// @Desc Test profile route
+// @Access Public route
 router.get('/test', (req: Request, res: Response) => res.json({ msg: 'Profile works' }));
 
 
