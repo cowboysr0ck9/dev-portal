a8836c4945ebc5820454e892fa6e5b9934f03f44
Tyler Eads <tyler.eads9@gmail.com>
2018-08-20 20:12:52 -0400

    Bootstrap4 Implementation and CDN Imports

--- client/src/views/MainView.tsx
+++ client/src/views/MainView.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import '../assets/scss/main.css';
+import '../assets/scss/main.scss';
+
+import Navbar from '../components/layout/Navbar';
+import Landing from '../components/layout/Landing';
+import Footer from '../components/layout/Footer';
 
 class MainView extends React.Component {
     public render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to
-                    reload.
-                </p>
+                <Navbar />
+                <Landing />
+                <Footer />
             </div>
         );
     }
