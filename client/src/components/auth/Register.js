65b502f2704a5383c4e7f4764dc94c77bd3c6521
Tyler Eads <tyler.eads9@gmail.com>
2018-11-23 10:27:00 -0500

    Register Page Update

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,118 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import ReactPasswordStrength from 'react-password-strength';
+import {
+    Container,
+    Row,
+    Col,
+    Button,
+    Form,
+    FormGroup,
+    Input,
+} from 'reactstrap';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {},
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
+        const { name, email, password, password2 } = this.state;
+
         return (
             <Container>
                 <Row>
-                    <Col>
-                        <h1>Register</h1>
+                    <Col md="8" className="m-auto">
+                        <h1 className="display-4 text-center">Register</h1>
+                        <p className="lead text-center">
+                            Create your new account
+                        </p>
+                        <Form>
+                            <FormGroup>
+                                <Input
+                                    type="text"
+                                    size="lg"
+                                    placeholder="Name"
+                                    name="name"
+                                    value={name}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input
+                                    size="lg"
+                                    type="email"
+                                    placeholder="Email Address"
+                                    name="email"
+                                    value={email}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input
+                                    type="password"
+                                    size="lg"
+                                    placeholder="Password"
+                                    name="password"
+                                    value={password}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <ReactPasswordStrength
+                                   
+                                    style={{ display: 'block' }}
+                                    minLength={8}
+                                    minScore={3}
+                                    scoreWords={[
+                                        'weak',
+                                        'okay',
+                                        'good',
+                                        'strong',
+                                        'stronger',
+                                    ]}
+                                    changeCallback={(e) => console.log(e)}
+                                    inputProps={{
+                                        name: 'password',
+                                        autoComplete: 'off',
+                                        className: 'form-control-lg',
+                                        placeholder: 'Password'
+                                    }}
+                                >
+
+                                </ReactPasswordStrength>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input
+                                    type="password"
+                                    size="lg"
+                                    placeholder="Confirm Password"
+                                    name="password2"
+                                    value={password2}
+                                    onChange={this.onChange}
+                                />
+                            </FormGroup>
+                            <Button
+                                size="lg"
+                                type="submit"
+                                color="primary"
+                                className="btn-block mt-4"
+                            >
+                                Submit
+                            </Button>
+                        </Form>
                     </Col>
                 </Row>
             </Container>
