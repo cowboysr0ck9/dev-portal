6232045de5c295ab38b327e6c86d65b8e8cce43a
Tyler Eads <tyler.eads9@gmail.com>
2019-07-28 11:06:05 -0400

    Login update

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,17 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
 class Login extends Component {
-  render() {
-    return (
-        <Container>
-        <Row>
-            <Col>
-                <h1>Login</h1>
-            </Col>
-        </Row>
-    </Container>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            errors: {},
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <Container>
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Log In</h1>
+                            <p className="lead text-center">
+                                Sign in to your DevPortal account
+                            </p>
+                            <form action="dashboard.html">
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email Address"
+                                        name="email"
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
+                                        name="password"
+                                    />
+                                </div>
+                                <input
+                                    type="submit"
+                                    className="btn btn-primary btn-block mt-4"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
 }
 export default Login;
