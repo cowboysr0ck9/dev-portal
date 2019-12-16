c41b2081d9d7bdf11797974a1657d0a1e6797bb9
Tyler Eads <tylereads@Tylers-MacBook-Pro.local>
2019-12-16 11:33:22 -0500

    Updates

--- src/routes/api/brand.ts
+++ src/routes/api/brand.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { Request, Response } from 'express';
 import passport from 'passport';
 
+import PDFDocument from 'pdfkit';
+import xlsx from 'xlsx';
+
 // Imports Models
 import Brand from '../models/Brand';
 
@@ -38,6 +41,40 @@ router.post('/', passport.authenticate('jwt', { session: false }), async (req: R
         return res.status(201).json(brand);
     } catch {
         return res.status(404).json({ message: 'Unable to create Brand.' });
+    }
+});
+
+// @Route GET api/brands/pdf
+router.get('/pdf', async (req: Request, res: Response) => {
+    try {
+        const pdf = new PDFDocument();
+        const file = encodeURIComponent('TestFileName') + '.pdf';
+
+        res.setHeader('Content-disposition', 'attachment; filename="' + file + '"');
+        res.setHeader('Content-type', 'application/pdf');
+
+        pdf.y = 300;
+        pdf.text('Test', 50, 50);
+        pdf.pipe(res);
+        pdf.end();
+    } catch {
+        return res.status(404).json({ message: 'Unable to create PDF.' });
+    }
+});
+
+// @Route GET api/brands/pdf
+router.get('/csv', async (req: Request, res: Response) => {
+    try {
+        const data = [['id', 'name', 'debits', 'credits']];
+        const ws = xlsx.utils.aoa_to_sheet(data);
+        const wb = xlsx.utils.book_new();
+        xlsx.utils.book_append_sheet(wb, ws, 'SheetJS');
+
+        const buffer = xlsx.write(wb, { type: 'buffer', bookType: 'xls' });
+
+        res.status(200).send(buffer);
+    } catch {
+        return res.status(404).json({ message: 'Unable to create PDF.' });
     }
 });
 
