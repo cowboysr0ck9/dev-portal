6bc869f7a9939dfa6447693a24bc1cf7764d5d4e
Tyler Eads <tylereads@Tylers-MacBook-Pro.local>
2019-12-30 14:44:20 -0500

    Filepath Casing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Navbar } from './components/layout/Navbar';
-import LandingPage from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import { Navbar } from './components/layout/navbar';
+import LandingPage from './components/layout/landing';
+import Register from './components/auth/register';
+import Login from './components/auth/login';
 import Alert from './components/alert/Alert';
 
 // Redux Store
